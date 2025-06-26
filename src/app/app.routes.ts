9a597d7f1dae72874431c436f9047afa572dab98
alfred32-dev <bytedev32@gmail.com>
2025-06-26 21:22:27 +0300

    Routing Implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './app/pages/dashboard/dashboard.component';
+import { TransactionsComponent } from './app/pages/transactions/transactions.component';
+import { TransferComponent } from './app/pages/transfer/transfer.component';
+import { ExportComponent } from './app/pages/export/export.component';
+import { SettingsComponent } from './app/pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  // Main Pages
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'transfer', component: TransferComponent },
+  { path: 'export', component: ExportComponent },
+  { path: 'settings', component: SettingsComponent },
+
+  // Fallback Route
+  { path: '**', redirectTo: '/dashboard' }
+];
